bfba5c84bab2ead3a195d852be24a3c4364701e6
Mario Alexis Uribe <umario284@gmail.com>
2022-07-14 15:30:40 -0500

    nuevo

--- ejercicios-2.js
+++ ejercicios-2.js
@@ -67,43 +67,4 @@ let dado = () => {
     
 }
 console.log(dado())
-var a = b = c = d = e = f = 0;
-        var totalTiradas = 0;
-          
-        function tiradaDado(){
-            var dado6caras = parseInt(6*Math.random()+1);
-            return dado6caras;
-        }
-        
-        for(i=1;i<=3600;i++){
-            tirada = tiradaDado();
-            switch(tirada){
-                case 1:
-                    a++;
-                    break;
-                case 2:
-                    b++;
-                    break;
-                case 3:
-                    c++;
-                    break;
-                case 4:
-                    d++;
-                    break;
-                case 5:
-                    e++;
-                    break;
-                case 6:
-                    f++;
-                    break;
-            }
-        }
-        totalTiradas = a+b+c+d+e+f;
-        
-        console.log("El 1 ha salido " +  a + " veces<br>");
-        console.log("El 2 ha salido " +  b + " veces<br>");
-        console.log("El 3 ha salido " +  c + " veces<br>");
-        console.log("El 4 ha salido " +  d + " veces<br>");
-        console.log("El 5 ha salido " +  e + " veces<br>");
-        console.log("El 6 ha salido " +  f + " veces<br>");
-        console.log("Total de lanzamientos del dado: " + totalTiradas);
+ 
